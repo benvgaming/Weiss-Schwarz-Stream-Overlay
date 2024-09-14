a561b7dcfc99337a1ddefce76e8019d768c9a88c
Diz310 <133223686+Diz310@users.noreply.github.com>
2024-09-14 19:27:07 +0200

    Added checks for ENG-Exclusives

--- WS_Stream_Overlay.py
+++ WS_Stream_Overlay.py
@@ -135,44 +135,63 @@ class Player1(ttk.Frame):
         self.canvas.itemconfig(self.playerbg, image=self.new_img)
 
     def save_key_card(self, event=None):
-        card_code = str(self.entry_key_card.get().lower())
-        char_remov = ["/", "-"]
-        for char in char_remov:
-            card_code = card_code.replace(char, "_")
-
-        if len(card_code) > 0:
-            set_initial = str(card_code[0])
-            first_underscore = card_code.find("_")
-            second_underscore = card_code.find("_", first_underscore + 1)
-            set_code = str(card_code[:second_underscore])
-            new_card = f"https://ws-tcg.com/wordpress/wp-content/images/cardlist/{set_initial}/{set_code}/{card_code}.png"
-
         try:
-            urllib.request.urlretrieve(new_card, "player_left.png")
+            card_code = str(self.entry_key_card.get().lower())
+            char_remov = ["/", "-"]
+            for char in char_remov:
+                card_code = card_code.replace(char, "_")
 
-            def make_hexagonal(im):
-                mask = Image.new("L", im.size, 0)
-                draw = ImageDraw.Draw(mask)
-                draw.regular_polygon(bounding_circle=(im.width // 2, im.width // 2, im.width // 2), n_sides=6, rotation=30, fill=255)
-                out = ImageOps.fit(im, mask.size, centering=(0, 0))
-                out.putalpha(mask)
-                return out
+            if len(card_code) > 0:
+                set_initial = str(card_code[0])
+                first_underscore = card_code.find("_")
+                second_underscore = card_code.find("_", first_underscore + 1)
+                set_code = str(card_code[:second_underscore])
+                card_id = str(card_code[first_underscore + 1:])
 
-            with Image.open("./player_left.png") as im:
-                adjust_size = (400, 559)
-                im = im.resize(adjust_size)
-                im = im.convert("RGBA")
-                im = im.crop((0, 0, im.width, im.width))
-                hex_img = make_hexagonal(im)
-                hex_img.save("./player_left_hex.png")
-                adjust_size = (148, 148)
-                hex_img = hex_img.resize(adjust_size)
-                hex_img.save("./player_left_hex.png")
-                os.remove("./player_left.png")
+                jp_card = f"https://ws-tcg.com/wordpress/wp-content/images/cardlist/{set_initial}/{set_code}/{card_code}.png"
+                normal_pattern_en = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/{set_initial}/{set_code}/{card_code.upper()}.png"
+                sds_2_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/SDS2/{card_code.upper()}.png"
+                ggst_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/GGST/{card_id.upper()}.png"
+                aotfs_td_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/AOTFS/TD/{card_code.upper()}.png"
+                aotfs_bp_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/AOTFS/BP/{card_code.upper()}.png"
+                rwby1_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/RWBY/{card_code.upper()}.png"
+                rwby2_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/RWBY/{card_id.upper()}.png"
+                atla_td_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/ATLA/TD/{card_code.upper()}.png"
+                atla_bp_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/ATLA/BP/{card_code.upper()}.png"
 
-            self.hex_img = tk.PhotoImage(file="./player_left_hex.png")
-            self.canvas.imgref = self.hex_img
-            self.canvas.itemconfig(self.playersetimg, image=self.hex_img)
+                list_of_urls = []
+                list_of_urls.extend((jp_card, normal_pattern_en, sds_2_card, ggst_card, aotfs_td_card, aotfs_bp_card, rwby1_card, rwby2_card, atla_td_card, atla_bp_card))
+
+                for url in list_of_urls:
+                    try:
+                        urllib.request.urlretrieve(url, "player_left.png")
+                        break
+                    except:
+                        continue
+
+                def make_hexagonal(im):
+                    mask = Image.new("L", im.size, 0)
+                    draw = ImageDraw.Draw(mask)
+                    draw.regular_polygon(bounding_circle=(im.width // 2, im.width // 2, im.width // 2), n_sides=6, rotation=30, fill=255)
+                    out = ImageOps.fit(im, mask.size, centering=(0, 0))
+                    out.putalpha(mask)
+                    return out
+
+                with Image.open("./player_left.png") as im:
+                    adjust_size = (400, 559)
+                    im = im.resize(adjust_size)
+                    im = im.convert("RGBA")
+                    im = im.crop((0, 0, im.width, im.width))
+                    hex_img = make_hexagonal(im)
+                    hex_img.save("./player_left_hex.png")
+                    adjust_size = (148, 148)
+                    hex_img = hex_img.resize(adjust_size)
+                    hex_img.save("./player_left_hex.png")
+                    os.remove("./player_left.png")
+
+                self.hex_img = tk.PhotoImage(file="./player_left_hex.png")
+                self.canvas.imgref = self.hex_img
+                self.canvas.itemconfig(self.playersetimg, image=self.hex_img)
 
         except:
             self.hex_img = tk.PhotoImage(file="./default_player_hex.png")
@@ -289,44 +308,63 @@ class Player2(ttk.Frame):
         self.canvas.itemconfig(self.playerbg, image=self.new_img)
 
     def save_key_card(self, event=None):
-        card_code = str(self.entry_key_card.get().lower())
-        char_remov = ["/", "-"]
-        for char in char_remov:
-            card_code = card_code.replace(char, "_")
-
-        if len(card_code) > 0:
-            set_initial = str(card_code[0])
-            first_underscore = card_code.find("_")
-            second_underscore = card_code.find("_", first_underscore + 1)
-            set_code = str(card_code[:second_underscore])
-            new_card = f"https://ws-tcg.com/wordpress/wp-content/images/cardlist/{set_initial}/{set_code}/{card_code}.png"
-
         try:
-            urllib.request.urlretrieve(new_card, "player_right.png")
+            card_code = str(self.entry_key_card.get().lower())
+            char_remov = ["/", "-"]
+            for char in char_remov:
+                card_code = card_code.replace(char, "_")
 
-            def make_hexagonal(im):
-                mask = Image.new("L", im.size, 0)
-                draw = ImageDraw.Draw(mask)
-                draw.regular_polygon(bounding_circle=(im.width // 2, im.width // 2, im.width // 2), n_sides=6, rotation=30, fill=255)
-                out = ImageOps.fit(im, mask.size, centering=(0, 0))
-                out.putalpha(mask)
-                return out
+            if len(card_code) > 0:
+                set_initial = str(card_code[0])
+                first_underscore = card_code.find("_")
+                second_underscore = card_code.find("_", first_underscore + 1)
+                set_code = str(card_code[:second_underscore])
+                card_id = str(card_code[first_underscore + 1:])
 
-            with Image.open("./player_right.png") as im:
-                adjust_size = (400, 559)
-                im = im.resize(adjust_size)
-                im = im.convert("RGBA")
-                im = im.crop((0, 0, im.width, im.width))
-                hex_img = make_hexagonal(im)
-                hex_img.save("./player_right_hex.png")
-                adjust_size = (148, 148)
-                hex_img = hex_img.resize(adjust_size)
-                hex_img.save("./player_right_hex.png")
-                os.remove("./player_right.png")
+                jp_card = f"https://ws-tcg.com/wordpress/wp-content/images/cardlist/{set_initial}/{set_code}/{card_code}.png"
+                normal_pattern_en = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/{set_initial}/{set_code}/{card_code.upper()}.png"
+                sds_2_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/SDS2/{card_code.upper()}.png"
+                ggst_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/GGST/{card_id.upper()}.png"
+                aotfs_td_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/AOTFS/TD/{card_code.upper()}.png"
+                aotfs_bp_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/AOTFS/BP/{card_code.upper()}.png"
+                rwby1_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/RWBY/{card_code.upper()}.png"
+                rwby2_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/RWBY/{card_id.upper()}.png"
+                atla_td_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/ATLA/TD/{card_code.upper()}.png"
+                atla_bp_card = f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/ATLA/BP/{card_code.upper()}.png"
 
-            self.hex_img = tk.PhotoImage(file="./player_right_hex.png")
-            self.canvas.imgref = self.hex_img
-            self.canvas.itemconfig(self.playersetimg, image=self.hex_img)
+                list_of_urls = []
+                list_of_urls.extend((jp_card, normal_pattern_en, sds_2_card, ggst_card, aotfs_td_card, aotfs_bp_card, rwby1_card, rwby2_card, atla_td_card, atla_bp_card))
+
+                for url in list_of_urls:
+                    try:
+                        urllib.request.urlretrieve(url, "player_right.png")
+                        break
+                    except:
+                        continue
+
+                def make_hexagonal(im):
+                    mask = Image.new("L", im.size, 0)
+                    draw = ImageDraw.Draw(mask)
+                    draw.regular_polygon(bounding_circle=(im.width // 2, im.width // 2, im.width // 2), n_sides=6, rotation=30, fill=255)
+                    out = ImageOps.fit(im, mask.size, centering=(0, 0))
+                    out.putalpha(mask)
+                    return out
+
+                with Image.open("./player_right.png") as im:
+                    adjust_size = (400, 559)
+                    im = im.resize(adjust_size)
+                    im = im.convert("RGBA")
+                    im = im.crop((0, 0, im.width, im.width))
+                    hex_img = make_hexagonal(im)
+                    hex_img.save("./player_right_hex.png")
+                    adjust_size = (148, 148)
+                    hex_img = hex_img.resize(adjust_size)
+                    hex_img.save("./player_right_hex.png")
+                    os.remove("./player_right.png")
+
+                self.hex_img = tk.PhotoImage(file="./player_right_hex.png")
+                self.canvas.imgref = self.hex_img
+                self.canvas.itemconfig(self.playersetimg, image=self.hex_img)
 
         except:
             self.hex_img = tk.PhotoImage(file="./default_player_hex.png")
